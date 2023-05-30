e0c0acb0aebf3e33f9b3b8f352460c393163acf7
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-30 16:50:13 +0200

    Add AppTitle component (and AppHero - bonus)

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppHero"
+}
+</script>
+
+<template>
+  <div>
+    <h2>Sono un Jumbotron</h2>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: azure;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h2 {
+  color: black;
+  font-size: 40px;
+}
+</style>
